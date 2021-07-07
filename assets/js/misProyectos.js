0f58a3b096abefbc70026f513b183aaaff473ff6
Lamayat <lamayat@unbosque.edu.co>
2021-07-07 16:26:07 -0500

    AAM - SE LOGRA FUNCIONAMIENTO BASICO DEL POPUP

--- assets/js/misProyectos.js
+++ assets/js/misProyectos.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-        
+     
+    //PROGRAMACIÓN PARA EL FILTRADO DE MIS PROYECTOS
     //Se agrega la clase active al primer enlace
     $('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
@@ -31,4 +32,29 @@ $(document).ready(function(){
             $('.project-item').css('transform', 'scale(1)');
         }setTimeout(showAll, 400);   
     });
+
+    //PROGRAMACION PARA LA PRESENTACION DE MIS PROYECTOS EN EL POPUP
+    // Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.project-list .project-item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
+
+	// Eventlistener del boton de cerrar
+	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+		overlay.classList.remove('activo');
+	});
+
+	// Eventlistener del overlay
+	overlay.addEventListener('click', (evento) => {
+		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+	});
+
 });
